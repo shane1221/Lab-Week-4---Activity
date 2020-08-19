0133f03b4283c910cc6b413d6ea749edf8e3eb76
shane1221 <61615677+shane1221@users.noreply.github.com>
2020-08-19 15:19:50 +1000

    100 percent complete

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -1,21 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
     int i = 0;
+    int rm = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rm = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
-     
-        Debug.Log(gameObject.name + ":" + i++);
+
+        Debug.Log(gameObject.name + ":" + i);
+        i++;
+
+
+        if (i == 100)
+            GameObject.FindWithTag("Red").active = false;
+
+        
+        
+        if (i == rm )
+            GameObject.FindWithTag("Blue").active = false;
     }
 }
