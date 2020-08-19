028d7bd521c5f636789ffb45c96e0495ab32332f
shane1221 <61615677+shane1221@users.noreply.github.com>
2020-08-19 14:15:50 +1000

    70 percent complete

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
 
+    [SerializeField]
+    private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
