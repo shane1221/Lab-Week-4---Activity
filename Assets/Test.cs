1d0bae976c6480446d6903c9b108af09780abc65
shane1221 <61615677+shane1221@users.noreply.github.com>
2020-08-19 13:15:08 +1000

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
